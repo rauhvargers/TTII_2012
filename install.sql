70581f50a39fb6a1877bc5b1cc12c5b17f4f710e
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2012-11-19 18:21:46 +0200

    added file uploads

--- install.sql
+++ install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 13, 2012 at 10:55 AM
+-- Generation Time: Nov 19, 2012 at 04:21 PM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.1
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `agenda` (
   `title` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
   `event_id` int(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `agenda`
@@ -35,7 +35,10 @@ INSERT INTO `agenda` (`id`, `title`, `event_id`) VALUES
 (1, 'Talking about the festival', 1),
 (2, 'Everything else', 1),
 (3, 'Waiting for THE moment', 2),
-(4, 'Drinking', 2);
+(4, 'Drinking', 2),
+(7, 'Svecings pie Daugavas', 11),
+(8, 'First exercise', 19),
+(9, 'Second exercise', 19);
 
 -- --------------------------------------------------------
 
@@ -73,20 +76,22 @@ CREATE TABLE IF NOT EXISTS `events` (
   `description` text COLLATE utf8_latvian_ci NOT NULL,
   `location_id` int(4) DEFAULT NULL,
   `start` datetime NOT NULL COMMENT 'The start date/time of the event',
+  `poster` varchar(255) COLLATE utf8_latvian_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=13 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`id`, `title`, `description`, `location_id`, `start`) VALUES
-(1, 'Festival organizers'' meeting', '', 1, '2012-11-28 10:30:00'),
-(2, 'New Year celebration', '', 2, '2012-12-31 21:00:00'),
-(9, 'Time for bed', '', 2, '2012-11-11 23:07:14'),
-(10, 'Lecture', 'Yet another lecture', 2, '2012-11-11 18:45:27'),
-(11, 'Lāčplēša diena', 'ufff... Bermonts', 1, '2013-11-11 21:00:00'),
-(12, 'Ziemassvētki', 'Tusiņš mājās', 1, '2012-12-24 19:00:00');
+INSERT INTO `events` (`id`, `title`, `description`, `location_id`, `start`, `poster`) VALUES
+(1, 'Festival organizers'' meeting', '', 1, '2012-11-28 10:30:00', NULL),
+(2, 'New Year celebration', '', 2, '2012-12-31 21:00:00', NULL),
+(9, 'Time for bed', '', 2, '2012-11-11 23:07:14', NULL),
+(10, 'Lecture', 'Yet another lecture', 2, '2012-11-11 18:45:27', NULL),
+(11, 'Lāčplēša diena', '<b>adfsdf sdfs dfsd</b>', 2, '2013-11-11 21:00:00', NULL),
+(12, 'Ziemassvētki', 'Tusiņš mājās', 1, '2012-12-24 19:00:00', NULL),
+(19, 'Second test, web technologies II', 'Very very serious test.', 1, '2012-12-11 10:30:00', 'KD1_uzdevumi.pdf');
 
 -- --------------------------------------------------------
 
@@ -127,3 +132,17 @@ CREATE TABLE IF NOT EXISTS `migration` (
 
 INSERT INTO `migration` (`type`, `name`, `migration`) VALUES
 ('app', 'default', '002_create_countries');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(4) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40) COLLATE utf8_latvian_ci NOT NULL,
+  `user_password` varchar(40) COLLATE utf8_latvian_ci NOT NULL,
+  `user_role` varchar(40) COLLATE utf8_latvian_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=1 ;
