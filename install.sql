3de2dea87604a4d629b8bf6a12a5c379d47981fa
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2012-11-21 07:26:26 +0200

    Updates to SQL installation script

--- install.sql
+++ install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2012 at 04:21 PM
+-- Generation Time: Nov 21, 2012 at 05:25 AM
 -- Server version: 5.1.44
 -- PHP Version: 5.3.1
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `agenda` (
   `title` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
   `event_id` int(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `agenda`
@@ -38,7 +38,8 @@ INSERT INTO `agenda` (`id`, `title`, `event_id`) VALUES
 (4, 'Drinking', 2),
 (7, 'Svecings pie Daugavas', 11),
 (8, 'First exercise', 19),
-(9, 'Second exercise', 19);
+(9, 'Second exercise', 19),
+(10, 'Some item', 21);
 
 -- --------------------------------------------------------
 
@@ -78,7 +79,7 @@ CREATE TABLE IF NOT EXISTS `events` (
   `start` datetime NOT NULL COMMENT 'The start date/time of the event',
   `poster` varchar(255) COLLATE utf8_latvian_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=21 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `events`
@@ -91,7 +92,9 @@ INSERT INTO `events` (`id`, `title`, `description`, `location_id`, `start`, `pos
 (10, 'Lecture', 'Yet another lecture', 2, '2012-11-11 18:45:27', NULL),
 (11, 'Lāčplēša diena', '<b>adfsdf sdfs dfsd</b>', 2, '2013-11-11 21:00:00', NULL),
 (12, 'Ziemassvētki', 'Tusiņš mājās', 1, '2012-12-24 19:00:00', NULL),
-(19, 'Second test, web technologies II', 'Very very serious test.', 1, '2012-12-11 10:30:00', 'KD1_uzdevumi.pdf');
+(22, 'Has attached file', 'Somethin', 1, '2012-11-21 00:00:00', '00_intro.pdf'),
+(19, 'Second test, web technologies II', 'Very very serious test.', 1, '2012-12-11 10:30:00', 'KD1_uzdevumi.pdf'),
+(21, '2nd Lecture <strong>today</strong>', 'Something bad is gonna happen', 1, '2012-11-20 10:31:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -136,13 +139,47 @@ INSERT INTO `migration` (`type`, `name`, `migration`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `passwordusers`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `passwordusers` (
   `user_id` int(4) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(40) COLLATE utf8_latvian_ci NOT NULL,
   `user_password` varchar(40) COLLATE utf8_latvian_ci NOT NULL,
   `user_role` varchar(40) COLLATE utf8_latvian_ci NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `passwordusers`
+--
+
+INSERT INTO `passwordusers` (`user_id`, `user_name`, `user_password`, `user_role`) VALUES
+(1, 'test', 'p@ssw0rd1', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_latvian_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `group` int(11) NOT NULL DEFAULT '1',
+  `email` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `last_login` varchar(25) COLLATE utf8_latvian_ci NOT NULL,
+  `login_hash` varchar(255) COLLATE utf8_latvian_ci NOT NULL,
+  `profile_fields` text COLLATE utf8_latvian_ci NOT NULL,
+  `created_at` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`,`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_latvian_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `group`, `email`, `last_login`, `login_hash`, `profile_fields`, `created_at`) VALUES
+(2, 'test', 'ZQSQ0tEBcgs9JWAuDU50hVaSMPSh1Km6ZcLcJdPSX/w=', 100, 'test@example.com', '1353360835', 'f83a35a28a9f1d48f83164cb60863574cba0a434', 'a:0:{}', 1353360631);
