f832527673b8518033dbc1c25d9fc3e2cb27780f
Krišs Rauhvargers <kriss.rauhvargers@lu.lv>
2013-11-26 00:58:04 +0200

    Was missing in previous commits

---
+++ fuel/app/views/demo/upload.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+	<form method="POST" enctype="multipart/form-data">
+	    <input type="file" name="demofile" />
+	    <input type="submit" value="Upload the file!" />
+	</form>
+    </body>
+</html>
